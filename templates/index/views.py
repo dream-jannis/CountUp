c9dc4b8c7853ca99da54e55d5fe3ddd0af4838d9
sus-jannis <jannis.stumpe@sussystems.de>
2023-08-19 11:40:11 +0200

    BarChart Optimation

--- templates/index/views.py
+++ templates/index/views.py
@@ -10,9 +10,13 @@ index = Blueprint("index", __name__, template_folder="pages")
 @index.route('/', methods=['GET', 'POST'])
 @login_required
 def main():
+
     if request.method == 'POST':
         user = request.form['user']
         event = request.form['event']
 
-
-    return render_template('index.html')
+    data = {
+        'labels': ['Label1', 'Label2', 'Label3'],
+        'counts': [5, 10, 15]
+    }
+    return render_template('index.html', data = data)
