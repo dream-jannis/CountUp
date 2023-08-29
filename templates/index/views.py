d0f65a42e46c721a22df5bacbdba012613761e3e
sus-jannis <jannis.stumpe@sussystems.de>
2023-08-29 21:05:58 +0200

    styling

--- templates/index/views.py
+++ templates/index/views.py
@@ -19,19 +19,23 @@ def main():
                 user_stroke[username] = len(read_strokes_by_username(username))
 
 
-    strokes_on_reservation = []
+    strokes_on_reservation_user_no_vote = []
+    strokes_on_reservation_remaining = []
     for item in read_all_strokes_on_reservation():
         if session["username"] not in (item["first_vote"],item["second_vote"], item["third_vote"]):
-            strokes_on_reservation.append(item)
+            strokes_on_reservation_user_no_vote.append(item)
+        else:
+            strokes_on_reservation_remaining.append(item)
 
-    count_strokes_reserveation = len(strokes_on_reservation)
+    count_strokes_reservation = len(strokes_on_reservation_user_no_vote)
 
     data = {
         'active_user': session['username'],
         'usernames': usernames,
         'user_stroke': user_stroke,
-        'count_open_strokes': count_strokes_reserveation,
-        'strokes_on_reservation': strokes_on_reservation
+        'count_open_strokes': count_strokes_reservation,
+        'strokes_on_reservation': strokes_on_reservation_user_no_vote,
+        'strokes_on_reservation_remaining': strokes_on_reservation_remaining
     }
     return render_template('index.html', data = data)
 
